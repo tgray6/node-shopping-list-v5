1701f9d9122aeb2f072f15c52e48aa692e521990
Benjamin White <benjamin.e.white1@gmail.com>
2017-05-16 11:28:44 -0400

    fix code

--- recipesRouter.js
+++ recipesRouter.js
@@ -69,12 +69,12 @@ router.put('/:id', jsonParser, (req, res) => {
     return res.status(400).send(message);
   }
   console.log(`Updating shopping list item \`${req.params.id}\``);
-  const updatedItem = Recipes.update({
+  Recipes.update({
     id: req.params.id,
     name: req.body.name,
     ingredients: req.body.ingredients
   });
-  res.status(204).json(updatedItem);
+  res.status(204).end();
 })
 
 module.exports = router;
