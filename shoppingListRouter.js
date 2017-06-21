8741d0f6d349f859fe593e66d931476158ce34a3
Benjamin White <benjamin.e.white1@gmail.com>
2017-06-21 15:44:35 -0400

    fix bug

--- shoppingListRouter.js
+++ shoppingListRouter.js
@@ -63,9 +63,8 @@ router.put('/:id', jsonParser, (req, res) => {
     }
   }
   if (req.params.id !== req.body.id) {
-    const message = (
-      `Request path id (${req.params.id}) and request body id `
-      `(${req.body.id}) must match`);
+    const message = `Request path id (${req.params.id}) and request body id
+      (${req.body.id}) must match`;
     console.error(message);
     return res.status(400).send(message);
   }
